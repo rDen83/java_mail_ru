c9d2dd18c2cfa2b2a840afc967105ccc225d58ae
rDen <rDen83@mail.ru>
2016-12-08 00:31:13 +0500

    v00
    Start test

---
+++ src/main/java/Main.java
@@ -0,0 +1,9 @@
+/**
+ * Created by Денис on 08.12.2016.
+ */
+public class Main {
+    public static void main(String[] args) throws Exception{
+      Frontend frontend = new Frontend();
+      
+    }
+}
